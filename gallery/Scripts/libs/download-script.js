21cd30012716d82557f46de3b8460ce467acdb18
Aleksey Prokofiev <Aleksei_Prokofev@epam.com>
2015-09-01 19:03:52 +0300

    no message

---
+++ gallery/Scripts/libs/download-script.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+
+    var i,
+        $items = $('#thumbCarousel .item'),
+        itemsLength = $items.length,
+        $itemsChildren = $items.children();
+
+    for (i = 0; i < itemsLength; i++) {
+        $($itemsChildren.clone().splice(i - 1, 1)).addClass('fade').prependTo($items[i]);
+        $($itemsChildren.clone().splice(i - 2, 1)).addClass('fade').prependTo($items[i]);
+        $($itemsChildren.clone().splice((i + 1) % itemsLength, 1)).addClass('fade').appendTo($items[i]);
+        $($itemsChildren.clone().splice((i + 2) % itemsLength, 1)).addClass('fade').appendTo($items[i]);
+    }
+
+    $('#bigCarousel').carousel({
+        interval: 10000
+    });
+
+    $('#thumbCarousel').carousel({
+        interval: 10000
+    });
+
+})();
---
+++ gallery/Scripts/libs/index-script.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    "use strict";
+
+    $("#datepicker").datepicker.defaults.format = "mm/dd/yy";
+    $("#datepicker").datepicker();
+    UUI.Panels.init();
+
+})();
